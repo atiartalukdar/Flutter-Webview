e7e409323eb7bb189bea78355790eb5a4eb6874b
atiartalukdar <atiartalukdar@gmail.com>
2021-12-21 18:01:09 +0600

    ---

--- lib/controller/app.controller.dart
+++ lib/controller/app.controller.dart
@@ -1,75 +1,92 @@
+import 'dart:async';
+import 'dart:developer';
 import 'dart:io';
 
+import 'package:location/location.dart';
 import 'package:weview/utils/export.util.dart';
 
 class AppController {
-  final GlobalKey webViewKey = GlobalKey();
-  late PullToRefreshController pullToRefreshController;
-  String url = "";
   double progress = 0;
-  final urlController = TextEditingController();
-  InAppWebViewController? webViewController;
+  late StreamSubscription subscription;
+  late bool isConnected;
+  late String platformVersion;
+  late LocationData locationData;
 
   InAppWebViewGroupOptions options = InAppWebViewGroupOptions(
-    crossPlatform: InAppWebViewOptions(
-      useShouldOverrideUrlLoading: true,
-      mediaPlaybackRequiresUserGesture: false,
-      clearCache: true,
-    ),
     android: AndroidInAppWebViewOptions(
       useHybridComposition: true,
-      clearSessionCache: true,
-      cacheMode: AndroidCacheMode.LOAD_NO_CACHE,
-    ),
-    ios: IOSInAppWebViewOptions(
-      allowsInlineMediaPlayback: true,
     ),
   );
 
-  void pullToRefresh() {
-    pullToRefreshController = PullToRefreshController(
-      options: PullToRefreshOptions(
-        color: Colors.blue,
-      ),
-      onRefresh: () async {
-        if (Platform.isAndroid) {
-          webViewController?.reload();
-        } else if (Platform.isIOS) {
-          webViewController?.loadUrl(urlRequest: URLRequest(url: await webViewController?.getUrl()));
+  Future<void> getInfo(InAppWebViewController controller) async {
+    String ip = await getIP();
+    await getLocation();
+    controller.addJavaScriptHandler(
+      handlerName: 'AjaxHandler',
+      callback: (args) {
+        switch (args[0].toString()) {
+          case "ip":
+            log('IP Address: ' + ip);
+            return {'getData': ip};
+
+          case "mac":
+            log('MAC Address: ' 'Mac Address: 00:0a:95:9d:68:16');
+            return {'getData': 'Mac Address: 00:0a:95:9d:68:16'};
+
+          case "location":
+            // log('Location: ' 'Location: ${getLocation()}');
+            return {'getData': 'Location: ${getLocation()}'};
+
+          case "connection":
+            log('Connection: ' + isConnected.toString());
+            return {'getData': isConnected.toString()};
         }
       },
     );
   }
 
-  appOnWebViewCreated(controller) {
-    webViewController = controller;
-    controller.addJavaScriptHandler(
-        handlerName: 'AndroidFunctions',
-        callback: (args) {
-          // print arguments coming from the JavaScript side!
-          // print(args);
-          if (args.toString() == "[CommonData]") {
-            // return data to the JavaScript side!
-            return {'MacAddress': '1597283465475_mKVFI3ShJd', 'IP': '192.168.7.164'};
-          }
-        });
+  Future<String> getIP() async {
+    String _ip = '';
+    for (var interface in await NetworkInterface.list()) {
+      for (var addr in interface.addresses) {
+        _ip = '${addr.address} ${addr.host}';
+      }
+    }
+    return _ip;
+  }
 
-    controller.addJavaScriptHandler(
-        handlerName: 'AjaxHandler',
-        callback: (args) {
-          // print arguments coming from the JavaScript side!
-          // log(args.toString());
+  Future<void> isConnectedToInternet(result) async {
+    if (result == ConnectivityResult.mobile || result == ConnectivityResult.wifi) {
+      isConnected = true;
+    } else {
+      isConnected = false;
+    }
+  }
 
-          switch (args.toString()) {
-            case "[AjaxSettingInfo]":
-              return {'getData': 'AjaxIntervalTime:500,AjaxRetry:20'};
+  Future<dynamic> getLocation() async {
+    Location location = Location();
 
-            case "[isConnectErrorMessage]":
-              return {'getData': 'false'};
+    bool _serviceEnabled;
+    PermissionStatus _permissionGranted;
 
-            case "[getIniUseVpn]":
-              return {'getData': '0'};
-          }
-        });
+    _serviceEnabled = await location.serviceEnabled();
+    if (!_serviceEnabled) {
+      _serviceEnabled = await location.requestService();
+      if (!_serviceEnabled) {
+        return;
+      }
+    }
+
+    _permissionGranted = await location.hasPermission();
+    if (_permissionGranted == PermissionStatus.denied) {
+      _permissionGranted = await location.requestPermission();
+      if (_permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    locationData = await location.getLocation();
+    log('location: $locationData');
+    // return locationData.toString();
   }
 }
